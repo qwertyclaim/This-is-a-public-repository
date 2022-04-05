43b000c165b0078435158b5c9d3a09129b84b544
qwertyclaim <qwertyclaim@yandex.ru>
2022-04-05 15:02:39 +0300

    Tasks will be added

---
+++ src/train.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import static java.lang.Math.pow;
+public class train {
+    public static void main(String[] args) {
+
+    }
+}
