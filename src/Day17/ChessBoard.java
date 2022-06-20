718e02690c19fdc8ee998bca17fe010989bab43d
Yoqwertyclaim@yandex.ru¹ <qwertyclaim@yandex.ru>
2022-06-20 15:15:43 +0300

    All recommendations were realized!

--- src/Day17/ChessBoard.java
+++ src/Day17/ChessBoard.java
@@ -5,7 +5,7 @@ public class ChessBoard {
     public void print(ChessPiece[][] cp) {
         for (int i = 0; i<cp.length; i++) {
             System.out.println("\n");
-            for (int j = 0; j<=7; j++) {
+            for (int j = 0; j<cp.length; j++) { // Указал вместо конкретной цифры итерацию до конца массива
                 System.out.print(cp[i][j].getSymbol());
             }
         }
--- src/Day17/Task2.java
+++ src/Day17/Task2.java
@@ -1,79 +1,45 @@
 package Day17;
+import java.util.Arrays;
 public class Task2 {
     public static void main(String[] args) {
         ChessPiece[][] cp = new ChessPiece[8][8];
+
+        for (ChessPiece[] row : cp)
+            Arrays.fill(row, ChessPiece.EMPTY);
+
         cp[0][0] = ChessPiece.ROOK_BLACK;
-        cp[0][1] = ChessPiece.EMPTY;
-        cp[0][2] = ChessPiece.EMPTY;
-        cp[0][3] = ChessPiece.EMPTY;
-        cp[0][4] = ChessPiece.EMPTY;
         cp[0][5] = ChessPiece.ROOK_BLACK;
         cp[0][6] = ChessPiece.KING_BLACK;
-        cp[0][7] = ChessPiece.EMPTY;
 
-        cp[1][0] = ChessPiece.EMPTY;
         cp[1][1] = ChessPiece.ROOK_WHITE;
-        cp[1][2] = ChessPiece.EMPTY;
-        cp[1][3] = ChessPiece.EMPTY;
         cp[1][4] = ChessPiece.PAWN_BLACK;
         cp[1][5] = ChessPiece.PAWN_BLACK;
-        cp[1][6] = ChessPiece.EMPTY;
         cp[1][7] = ChessPiece.PAWN_BLACK;
 
         cp[2][0] = ChessPiece.PAWN_BLACK;
-        cp[2][1] = ChessPiece.EMPTY;
         cp[2][2] = ChessPiece.KNIGHT_BLACK;
-        cp[2][3] = ChessPiece.EMPTY;
-        cp[2][4] = ChessPiece.EMPTY;
-        cp[2][5] = ChessPiece.EMPTY;
         cp[2][6] = ChessPiece.PAWN_BLACK;
-        cp[2][7] = ChessPiece.EMPTY;
 
         cp[3][0] = ChessPiece.QUEEN_BLACK;
-        cp[3][1] = ChessPiece.EMPTY;
-        cp[3][2] = ChessPiece.EMPTY;
         cp[3][3] = ChessPiece.BISHOP_WHITE;
-        cp[3][4] = ChessPiece.EMPTY;
-        cp[3][5] = ChessPiece.EMPTY;
-        cp[3][6] = ChessPiece.EMPTY;
-        cp[3][7] = ChessPiece.EMPTY;
 
-        cp[4][0] = ChessPiece.EMPTY;
-        cp[4][1] = ChessPiece.EMPTY;
-        cp[4][2] = ChessPiece.EMPTY;
         cp[4][3] = ChessPiece.BISHOP_BLACK;
         cp[4][4] = ChessPiece.PAWN_WHITE;
-        cp[4][5] = ChessPiece.EMPTY;
-        cp[4][6] = ChessPiece.EMPTY;
-        cp[4][7] = ChessPiece.EMPTY;
 
-        cp[5][0] = ChessPiece.EMPTY;
-        cp[5][1] = ChessPiece.EMPTY;
-        cp[5][2] = ChessPiece.EMPTY;
-        cp[5][3] = ChessPiece.EMPTY;
         cp[5][4] = ChessPiece.BISHOP_WHITE;
         cp[5][5] = ChessPiece.PAWN_WHITE;
-        cp[5][6] = ChessPiece.EMPTY;
-        cp[5][7] = ChessPiece.EMPTY;
 
         cp[6][0] = ChessPiece.PAWN_WHITE;
-        cp[6][1] = ChessPiece.EMPTY;
-        cp[6][2] = ChessPiece.EMPTY;
         cp[6][3] = ChessPiece.QUEEN_WHITE;
-        cp[6][4] = ChessPiece.EMPTY;
         cp[6][5] = ChessPiece.PAWN_WHITE;
-        cp[6][6] = ChessPiece.EMPTY;
         cp[6][7] = ChessPiece.PAWN_WHITE;
 
-        cp[7][0] = ChessPiece.EMPTY;
-        cp[7][1] = ChessPiece.EMPTY;
-        cp[7][2] = ChessPiece.EMPTY;
-        cp[7][3] = ChessPiece.EMPTY;
-        cp[7][4] = ChessPiece.EMPTY;
         cp[7][5] = ChessPiece.ROOK_WHITE;
         cp[7][6] = ChessPiece.KING_WHITE;
-        cp[7][7] = ChessPiece.EMPTY;
+
         ChessBoard cb = new ChessBoard(cp);
         cb.print(cp);
     }
 }
+/* Текущие изменения: реализован метод "fill" для первоначльного проставления пустых ячеек на шахматной доске;
+* далее заполнение доски фигурами происходит вручную */
